3313ffba5af66ee6284ab9692ceba60d44a18174
Yomna-Raouf <yomnaraouf99@gmail.com>
2024-01-12 10:21:07 +0200

    chore: initialize angular app

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    fontFamily: {
+      inter: ["Inter", "ui-sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
