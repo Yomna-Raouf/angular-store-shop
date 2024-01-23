9b8f3c1e859e35b87e0c351ba0d232b73b3b5994
Yomna-Raouf <yomnaraouf99@gmail.com>
2024-01-23 07:50:23 +0200

    chore: remove products routing module

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ProductsRoutingModule } from './products-routing.module';
-
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [ProductsRoutingModule]
 })
 export class ProductsModule { }
